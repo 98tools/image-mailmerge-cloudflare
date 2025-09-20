c3384716973009b8f3cd4243195aafd4dcf4948d
Abdulkarim Lahmuni <abdulkarim1422.18@gmail.com>
2025-09-20 12:54:28 +0300

    feat: implement Header component and integrate it into main layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ImageMailMerge from './ImageMailMerge'
+import Header from './header'
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ImageMailMerge />
+    <div className="h-screen bg-gray-900 text-white overflow-hidden">
+      <Header />
+      <ImageMailMerge />
+    </div>
   </React.StrictMode>,
 ) 
